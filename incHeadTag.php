7d7261e157aa8701e57192ae96f1d78a8c1e91f9
robstarkdev <robstarkdev@gmail.com>
2021-06-03 21:19:47 -0400

    fix logo size on apple

--- incHeadTag.php
+++ incHeadTag.php
@@ -3,6 +3,6 @@
 
 ?>
 
-<link rel="stylesheet" type="text/css" media="screen" href="cssmain.css">
+<link rel="stylesheet" type="text/css" media="screen" href="cssmain.css?vers=2">
 <link rel="icon" href="images/logo_for_website.png">
 
